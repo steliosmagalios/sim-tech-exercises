a1dbad7def75e29677bdd6963755ee72b5994f7f
Stelios Magalios <stelios.magalios1@gmail.com>
2022-10-23 21:19:33 +0300

    tests 2, 3 and 5 done

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() {
 
 /// Test 1: Display the first 100 random numbers
 fn test_1() {
-    let mut prng = Generator::new_with_seed(0);
+    let mut prng: Generator = Generator::new_with_seed(0);
 
     for i in 0..100 {
         println!("{:3}: {:.20}", i + 1, prng.next_random())
@@ -43,20 +43,79 @@ fn test_1() {
 
 /// Test 2: Mean and Standard Deviation of first 100 numbers
 fn test_2() {
-    let mut prng = Generator::new_with_seed(0);
+    let mut prng: Generator = Generator::new_with_seed(0);
+
+    let mut mean: f64 = 0.0;
+    let mut ssq: f64 = 0.0;
+    let mut sqs: f64 = 0.0;
+
+    for _ in 0..100 {
+        let curr_num: f64 = prng.next_random();
+
+        // Calculate mean
+        mean += curr_num / 100.0;
+
+        // Calculate variables needed for standard deviation
+        // Thanks to this math.stackexchange answer fot the formula
+        // https://math.stackexchange.com/q/3045127
+        ssq += curr_num.powf(2.0);
+        sqs += curr_num;
+    }
+
+    let standard_deviation: f64 = (ssq / 100.0 - (sqs / 100.0).powf(2.0)).sqrt();
+
+    println!("              Mean: {}", mean);
+    println!("Standard Deviation: {}", standard_deviation);
 }
 
 /// Test 3: Mean and Standard Deviation of first 1,000,000 numbers
 fn test_3() {
-    let mut prng = Generator::new_with_seed(0);
+    let mut prng: Generator = Generator::new_with_seed(0);
+
+    let mut mean: f64 = 0.0;
+    let mut ssq: f64 = 0.0;
+    let mut sqs: f64 = 0.0;
+
+    for _ in 0..1_000_000 {
+        let curr_num: f64 = prng.next_random();
+
+        // Calculate mean
+        mean += curr_num / 1_000_000.0;
+
+        // Calculate variables needed for standard deviation
+        // Thanks to this math.stackexchange answer fot the formula
+        // https://math.stackexchange.com/q/3045127
+        ssq += curr_num.powf(2.0);
+        sqs += curr_num;
+    }
+
+    let standard_deviation: f64 = (ssq / 1_000_000.0 - (sqs / 1_000_000.0).powf(2.0)).sqrt();
+
+    println!("              Mean: {}", mean);
+    println!("Standard Deviation: {}", standard_deviation);
 }
 
 /// Test 4: Runs test for runs 1 through 10
 fn test_4() {
-    let mut prng = Generator::new_with_seed(0);
+    // let mut prng: Generator = Generator::new_with_seed(0);
 }
 
 /// Test 5: Regions test for 10 regions
 fn test_5() {
-    let mut prng = Generator::new_with_seed(0);
+    let mut prng: Generator = Generator::new_with_seed(0);
+    let mut regions: [u64; 10] = [0; 10];
+
+    for _ in 0..1_000_000 {
+        // Get the region by multiplying and the taking only the
+        // decimal part. For 10 regions, this sould go from 0 to 9.
+        let region: usize = (prng.next_random() * 10.0) as usize;
+
+        // Increase the region count
+        regions[region] += 1;
+    }
+
+    println!("{:^15} {:^15}", "Region", "Probability");
+    for (i, occurencies) in regions.iter().enumerate() {
+        println!("{:^15} {:^15.6}", i, *occurencies as f64 / 1_000_000.0);
+    }
 }
